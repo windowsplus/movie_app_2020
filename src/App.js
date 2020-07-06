f143c2fe8f68a227df89140b9465941b7bd0e052
caleb <shlee79@gmail.com>
2020-07-06 16:34:11 +0900

    movie 시작

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import React from "react";
+import axios from "axios";
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
-
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-
-  add = () => {
-    this.setState((current) => ({ count: this.state.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: this.state.count - 1 }));
-  };
-
   componentDidMount() {
-    console.log("component rendered");
-  }
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 export default App;
